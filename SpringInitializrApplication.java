e52862661b3778c26af9ff0eaf3ed9c5a2f289d6
Atharva Parkar <atharvaparkar410@gmail.com>
2026-02-01 09:48:37 +0530

    Update SpringInitializrApplication.java

--- SpringInitializrApplication.java
+++ SpringInitializrApplication.java
@@ -1,6 +1,5 @@
 package org.example;
-
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; //Helper Class
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
